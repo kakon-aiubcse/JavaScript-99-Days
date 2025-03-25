90896fde19e2f4c2e18d07742b075470f16c9bab
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-03-26 02:29:40 +0600

    checked synchronous programs

--- Day-13/index.js
+++ Day-13/index.js
@@ -3,16 +3,24 @@
 //callbacks, promises and async awaits
 //trycatch block 
 //suppose you want to complete one execution and then another but in the middle you get an error then the remaining code will stop working so if you want to catch that issue then we can use try catch block 
- console.log("first printing");try{
+//  console.log("first printing");try{
  
-  console.lg("second printing");
+//   console.lg("second printing");
   
-}catch(error){
-console.log(error);
+// }catch(error){
+// console.log(error);
 
-}console.log("third printing");
+// }console.log("third printing");
 //output
 // first printing
 // index.js:11 TypeError: console.lg is not a function
 //     at index.js:8:11
 // index.js:13 third printing
+
+//synchronous programs
+{
+  //synchronous is like doing one execution then another then another one by one is call synchronous
+  console.log("one");
+  console.log("two");
+  console.log("three");
+}
