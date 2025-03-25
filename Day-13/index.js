63b16211e300d42b1f9d9d7739013c1559924c79
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-03-26 02:26:03 +0600

    try-catch block learnt

---
+++ Day-13/index.js
@@ -0,0 +1,18 @@
+//Day 13
+// Javascript 99days
+//callbacks, promises and async awaits
+//trycatch block 
+//suppose you want to complete one execution and then another but in the middle you get an error then the remaining code will stop working so if you want to catch that issue then we can use try catch block 
+ console.log("first printing");try{
+ 
+  console.lg("second printing");
+  
+}catch(error){
+console.log(error);
+
+}console.log("third printing");
+//output
+// first printing
+// index.js:11 TypeError: console.lg is not a function
+//     at index.js:8:11
+// index.js:13 third printing
