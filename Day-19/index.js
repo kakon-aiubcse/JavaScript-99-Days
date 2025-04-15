58e1f48a60f3c0721d9d1e28bd618b4e6c04c797
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-15 08:38:26 +0600

    learnt object destructing and completed part-1

--- Day-19/index.js
+++ Day-19/index.js
@@ -1,21 +1,22 @@
 //Day 19
 //learning Modern javascript ES6+ Features by Mosh.
 //prev function creation
+//part-1
 const square = function (number) {
   return number * number;
 };
 let res = square(2);
 console.log(res);
-//ones6+
+//on es6+
 const square2 = (number) => number * number;
 //if single parameter if no parameter then () and directly can be returned
 console.log(square2(4));
 
 //
 const man = [
-  { id: 1, person: 1, name: "Khairul ", age: 15 },
+  { id: 1, person: 1, name: "Khairul", age: 15 },
   { id: 2, person: 1, name: "Islam", age: 16 },
-  { id: 3, person: 1, name: "kakon", age: 17 },
+  { id: 3, person: 1, name: "Kakon", age: 17 },
 ];
 
 const prevdata = man.filter(function (res) {
@@ -25,3 +26,19 @@ console.log(prevdata);
 //in es6+
 const newdata = man.filter((res) => res.id);
 console.log(newdata);
+
+//template literal
+const newdata2 = man.map((res) => `data myself: ${JSON.stringify(res)}`);
+console.log(newdata2);
+
+//object destructure
+const people = {
+  name: "kakon",
+  country: "Bangladesh",
+
+};
+const {name , country } = people;
+console.log(name, country);
+//adding new variable to access 
+const {name: nm, country:ct } = people;
+console.log(nm, ct);
