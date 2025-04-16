460536957f481aefe37a5481dfb8ba9fb6bf90e1
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-16 10:27:52 +0600

    learnt spread operator

--- Day-20/index.js
+++ Day-20/index.js
@@ -5,3 +5,20 @@
 
 //spread operator for arrays
 
+let data = [1, 2, 3];
+let response = ["kakon", "khairul"];
+
+let combine = [...data, "Islam", 2, ...response];
+console.log(combine);
+
+//for objects
+let dataobj = {
+  name: "kakon",
+  occupation: "job seeker",
+};
+let responseobj = {
+  gotJob: false,
+};
+
+let objcombine = { ...dataobj, stillsearching: "yes", ...responseobj };
+console.log(objcombine);
