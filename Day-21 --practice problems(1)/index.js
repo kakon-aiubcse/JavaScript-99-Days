fc15a82bc69b38f07962373965b366823211ca78
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-17 07:15:19 +0600

    completed solving practice problem of a function to reverse a string

--- Day-21 --practice problems(1)/index.js
+++ Day-21 --practice problems(1)/index.js
@@ -1,3 +1,24 @@
-//Day 21 
+//Day 21
 //Practice problems 01
 //Write a function to reverse a string
+
+
+//approach -1 using loop
+let str = "Kakon";
+function reversing(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  console.log(reversed);
+}
+reversing(str);
+
+//approach -2 using methods
+
+
+let strng = "Khairul";
+function reverestrng (strng){
+    return strng.split('').reverse().join('');
+}
+console.log(reverestrng(strng));
