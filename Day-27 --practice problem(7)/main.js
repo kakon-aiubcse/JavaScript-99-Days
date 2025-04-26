dc87574d30850f2e55b87058613aca3a8d5f4545
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-26 09:06:39 +0600

    problem solving --7 on capitalizing the first letter of each word in a sentence

---
+++ Day-27 --practice problem(7)/main.js
@@ -0,0 +1,15 @@
+//practice problem 06
+//Write a program to capitalize the first letter of each word in a sentence
+
+let arr = ["this is a sentence"];
+
+function capletter(arr) {
+  let words = arr[0].split(" ");
+  for (let i = 0; i < words.length; i++) {
+    let capword = words[i];
+    words[i] = capword[0].toUpperCase() + capword.slice(1);
+  }
+  return words.join(" ");
+}
+
+console.log(capletter(arr));
