5c114ee942b55e3a5fb1873644f5e5043f4da595
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-28 10:22:59 +0600

    implemented fizzbuzz logic from 1 to 100 with if else condition

---
+++ Day-29 --practice problem(9)/main.js
@@ -0,0 +1,21 @@
+//practice problem 09
+//Implement a fizzBuzz logic from 1 to 100
+//If a number can be divided by 3, say Fizz. If by 5, say Buzz. If by both 3 and 5, say FizzBuzz. Otherwise, just say the number.
+
+
+//using for loop and if else
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz", i);
+    } else if (i % 3 === 0) {
+      console.log("Fizz", i);
+    } else if (i % 5 === 0) {
+      console.log("Buzz", i);
+    } else {
+      console.log(i);
+    }
+  }
+}
+fizzBuzz();
+
