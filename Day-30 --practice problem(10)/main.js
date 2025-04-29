70d1e328325260174481d3d3f4e24a876f5bf281
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-29 16:14:45 +0600

    problem solved on returning second largest number in an array

--- Day-30 --practice problem(10)/main.js
+++ Day-30 --practice problem(10)/main.js
@@ -1,11 +1,36 @@
 //Return the second largest number from an array
 
-let numbers = [23, 224, 2, 22, 44,56 , 100];
+// let numbers = [23, 224, 2, 22, 44,56 , 100];
 
-function secondhighestnum(numbers) {
-  let sorted = numbers.sort((a, b) => b - a);
-  console.log(`Given Array: ${numbers} \narray shorted from highest to lowest ${sorted} \nso the second largest number is : ${sorted[1]}`) 
-  
+// function secondhighestnum(numbers) {
+//   let sorted = numbers.sort((a, b) => b - a);
+//   console.log(`Given Array: ${numbers} \narray shorted from highest to lowest ${sorted} \nso the second largest number is : ${sorted[1]}`)
+
+// }
+
+// secondhighestnum(numbers);
+
+//second approach
+
+//loop on each
+//take the greater one top
+//swap the greater one to another greater one
+//make a list of sorted higher to lower
+let array = [23, 224, 2, 22, 44, 56, 100];
+function sortingHtoL(array) {
+  let result= [...array]
+  let temp;
+
+  for (let i = 0; i < result.length; i++) {
+    for (let j = i + 1; j < result.length; j++) {
+      if (result[j] > result[i]) {
+        temp = result[i]; //temp =23 
+        result[i] = result[j]; //23 turns into 224 || 23 turns into 44 || 
+        result[j] = temp; // temp now 224 || temp now adds 44
+      }
+    }
+    
+  }console.log(result);
+  console.log(result[1]);
 }
-
-secondhighestnum(numbers);
+sortingHtoL(array);
